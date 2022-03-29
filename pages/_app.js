ea253f790a8cd16c10f58a68b513a55e63f01c08
iKevi <31235514+iKevi@users.noreply.github.com>
2022-03-29 09:41:38 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import Fonts from '../components/fonts'
+import theme from '../lib/theme'
+import '../components/css/embla.css'
+// pages/_app.js
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../lib/styles.css'
+
+
+import { AnimatePresence } from 'framer-motion'
+
+function Website({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default Website
